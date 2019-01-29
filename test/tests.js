824f4c7725ca53f6d6b36015fcc9c715936f0dfe
emily baier <emilyrbaier@gmail.com>
2019-01-29 15:27:18 -0800

    added fizz and buzz variables

--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,21 @@
 const test = QUnit.test;
 
+let fizz = 'fizz';
+let buzz = 'buzz';
+
 function fizzBuzz(num) {
 
     if(num === 100) {
         return 'YOU ARE SO SPECIAL';
     }
     else if(num % 9 === 0 && num % 4 === 0) {
-        return num + ' fizzbuzz';
+        return num + ' ' + fizz + buzz;
     }
     else if(num % 9 === 0) {
-        return num + ' fizz';
+        return num + ' ' + fizz;
     }
     else if(num % 4 === 0) {
-        return num + ' buzz';
+        return num + ' ' + buzz;
     }
     else {
         return num;
@@ -28,13 +31,13 @@ test('if num is 100, return YOU ARE SO SPECIAL', function(assert) {
 test('if divisible by 9 return number and fizz', function(assert) {
     let num = 9;
     let result = fizzBuzz(num);
-    assert.equal(result, num + ' fizz');
+    assert.equal(result, num + ' ' + fizz);
 });
 
 test('if divisible by 4 return number and buzz', function(assert) {
     let num = 4; 
     let result = fizzBuzz(num);
-    assert.equal(result, num + ' buzz');
+    assert.equal(result, num + ' ' + buzz);
 });
 
 test('if not divisible by 9 or 4, return number', function(assert) {
@@ -46,7 +49,7 @@ test('if not divisible by 9 or 4, return number', function(assert) {
 test('if divisible by 4 and 9, return number and fizzbuzz', function(assert) {
     let num = 36;
     let result = fizzBuzz(num);
-    assert.equal(result, num + ' fizzbuzz');
+    assert.equal(result, num + ' ' + fizz + buzz);
 });
 
 
